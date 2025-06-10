944658ed6bd256695248ec45d0285c1faca534e6
youngIcom <yesayasentosa@gmail.com>
2025-06-10 23:51:56 +0700

    program final : perbaikan pada arah belok

--- tugas_final/backend_sistem_monitor.ino
+++ tugas_final/backend_sistem_monitor.ino
@@ -21,12 +21,6 @@
 // Pin untuk Servo
 #define SERVO_PIN 2
 
-// Tidak lagi menggunakan ledc, jadi konstanta PWM channel, freq, resolution tidak diperlukan
-// #define PWM_CHANNEL_A 0
-// #define PWM_CHANNEL_B 1
-// #define PWM_FREQ 1000
-// #define PWM_RESOLUTION 8
-
 Servo myServo;
 
 // Server WiFi
@@ -34,9 +28,9 @@ WiFiServer server(8080); // Server akan listen di port 8080
 WiFiClient client;       // Merepresentasikan client yang terhubung
 
 // Variabel untuk menyimpan data
-String buff; 
+String buff;
 const char delimiter[] = ":";
-int motor_speed_val, servo_angle_val; 
+int motor_speed_val, servo_angle_val;
 int sensor1, sensor2, sensor3, sensor4, sensor5;
 
 void setup() {
@@ -53,7 +47,7 @@ void setup() {
   // Set pin sensor sebagai INPUT
   pinMode(S1, INPUT);
   pinMode(S2, INPUT);
-  pinMode(S3, INPUT); 
+  pinMode(S3, INPUT);
   pinMode(S4, INPUT);
   pinMode(S5, INPUT);
 
@@ -64,59 +58,60 @@ void setup() {
   // WiFiManager
   WiFiManager wm;
   bool res;
-  res = wm.autoConnect("AutoConnectAP","password"); 
+  res = wm.autoConnect("AutoConnectAP","password");
 
   if (!res) {
     Serial.println("Gagal terhubung ke WiFi");
   } else {
     Serial.println("WiFi terhubung!");
     Serial.print("Alamat IP: ");
-    Serial.println(WiFi.localIP()); 
-    server.begin(); 
-    Serial.println("Server TCP dimulai di port 8080. Menunggu client...");
+    Serial.println(WiFi.localIP());
+    server.begin();
+    Serial.println("Server TCP dimulai. Menunggu client...");
   }
 }
 
 void loop() {
-  sensor(); 
+  sensor();
 
   if (server.hasClient()) {
     if (client && client.connected()) {
-        Serial.println("Client baru mencoba terhubung, memutuskan client lama.");
+        Serial.println("Client baru mencoba terhubung, memutuskan client
+lama.");
         client.stop();
     }
-    client = server.available(); 
+    client = server.available();
     if (client) {
         Serial.println("Client baru terhubung!");
-        client.flush(); 
+        client.flush();
     }
   }
 
   if (client && client.connected()) {
     if (client.available() > 0) {
-      buff = client.readStringUntil('\n'); 
-      buff.trim(); 
-      Serial.print("Diterima dari client: "); 
+      buff = client.readStringUntil('\n');
+      buff.trim();
+      Serial.print("Diterima dari client: ");
       Serial.println(buff);
 
       if (buff.length() > 0) {
         if (buff[0] == 'M') {
-          char buff_array[buff.length() + 1]; 
+          char buff_array[buff.length() + 1];
           buff.toCharArray(buff_array, sizeof(buff_array));
-          
-          char *token = strtok(buff_array, delimiter); 
+
+          char *token = strtok(buff_array, delimiter);
           if (token != NULL) {
-            token = strtok(NULL, delimiter); 
+            token = strtok(NULL, delimiter);
             if (token != NULL) {
-              motor_speed_val = atoi(token); 
+              motor_speed_val = atoi(token);
             } else {
-              motor_speed_val = 0; 
+              motor_speed_val = 0;
             }
           } else {
-            motor_speed_val = 0; 
+            motor_speed_val = 0;
           }
 
-          char *action_token = strtok(NULL, delimiter); 
+          char *action_token = strtok(NULL, delimiter);
           if (action_token != NULL && strlen(action_token) > 0) {
             char action = action_token[0];
             if (action == 'F') {
@@ -131,60 +126,61 @@ void loop() {
               stopMotors();
             } else {
               Serial.println("Aksi motor tidak dikenal.");
-              stopMotors(); 
+              stopMotors();
             }
           } else {
             Serial.println("Aksi motor tidak ada, memberhentikan motor.");
-            stopMotors(); 
+            stopMotors();
           }
-        } 
+        }
         else if (buff[0] == 'S') {
           char buff_array[buff.length() + 1];
           buff.toCharArray(buff_array, sizeof(buff_array));
-          
-          char *token = strtok(buff_array, delimiter); 
+
+          char *token = strtok(buff_array, delimiter);
           if (token != NULL) {
-            token = strtok(NULL, delimiter); 
+            token = strtok(NULL, delimiter);
             if (token != NULL) {
-              servo_angle_val = atoi(token); 
+              servo_angle_val = atoi(token);
               myServo.write(servo_angle_val);
-              Serial.print("Servo diatur ke: "); Serial.println(servo_angle_val);
+              Serial.print("Servo diatur ke: "); Serial.println
+(servo_angle_val);
             }
           }
-        } 
+        }
         else if (buff.startsWith("GETSENSOR")) {
           String sensorData = "SENSORS:";
           sensorData += String(sensor1) + ",";
           sensorData += String(sensor2) + ",";
-          sensorData += String(sensor3) + ","; 
+          sensorData += String(sensor3) + ",";
           sensorData += String(sensor4) + ",";
           sensorData += String(sensor5);
-          client.println(sensorData); 
-          Serial.print("Dikirim ke client: "); 
+          client.println(sensorData);
+          Serial.print("Dikirim ke client: ");
           Serial.println(sensorData);
-        } 
+        }
         else {
             Serial.print("Perintah tidak dikenal: ");
             Serial.println(buff);
         }
       }
     }
-  } 
+  }
 }
 
 void sensor() {
   sensor1 = digitalRead(S1);
   sensor2 = digitalRead(S2);
-  sensor3 = analogRead(S3); 
+  sensor3 = analogRead(S3);
   sensor4 = digitalRead(S4);
   sensor5 = digitalRead(S5);
 }
 
 // Fungsi kontrol motor menggunakan analogWrite
 void forward(int speed_val) {
-  digitalWrite(IN1, HIGH); 
+  digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH); 
+  digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
   analogWrite(ENA, speed_val); // Menggunakan analogWrite
   analogWrite(ENB, speed_val); // Menggunakan analogWrite
@@ -192,33 +188,35 @@ void forward(int speed_val) {
 }
 
 void backward(int speed_val) {
-  digitalWrite(IN1, LOW);  
+  digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
-  digitalWrite(IN3, LOW);  
+  digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
   analogWrite(ENA, speed_val); // Menggunakan analogWrite
   analogWrite(ENB, speed_val); // Menggunakan analogWrite
   Serial.print("Motor: Mundur, Kecepatan: "); Serial.println(speed_val);
 }
 
-void turnLeft(int speed_val) {
-  digitalWrite(IN1, HIGH); 
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, LOW);  
+void turnRight(int speed_val) {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENA, speed_val); // Menggunakan analogWrite
-  analogWrite(ENB, 0);         // Menggunakan analogWrite
-  Serial.print("Motor: Belok Kiri, Kecepatan Kiri: "); Serial.println(speed_val);
+  analogWrite(ENA, speed_val);
+  analogWrite(ENB, speed_val);
+  Serial.print("Motor: Belok Kanan (pivot), Kecepatan: "); Serial.println
+(speed_val);
 }
 
-void turnRight(int speed_val) {
-  digitalWrite(IN1, LOW);  
+void turnLeft(int speed_val) {
+  digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH); 
-  digitalWrite(IN4, LOW);
-  analogWrite(ENA, 0);         // Menggunakan analogWrite
-  analogWrite(ENB, speed_val); // Menggunakan analogWrite
-  Serial.print("Motor: Belok Kanan, Kecepatan Kanan: "); Serial.println(speed_val);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENA, speed_val);
+  analogWrite(ENB, speed_val);
+  Serial.print("Motor: Belok Kiri (pivot), Kecepatan: "); Serial.println
+(speed_val);
 }
 
 void stopMotors() {
